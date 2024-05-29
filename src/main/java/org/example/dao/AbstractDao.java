5471409a71314962da47dae25efac98e68dd9c70
Gaban <gabin.deboulogne@gmail.com>
2024-05-29 10:13:32 +0200

    Initial commit

---
+++ src/main/java/org/example/dao/AbstractDao.java
@@ -0,0 +1,71 @@
+package org.example.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class AbstractDao<T>
+{
+	private final SessionFactory sessionFactory;
+	Class<T> c;
+
+	public AbstractDao(SessionFactory sessionFactory, Class<T> c)
+	{
+		this.sessionFactory = sessionFactory;
+		this.c = c;
+	}
+
+	public void saveOrUpdate(T entity)
+	{
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.saveOrUpdate(entity);
+		transaction.commit();
+		session.close();
+	}
+
+	public T findById(Long id)
+	{
+		Session session = sessionFactory.openSession();
+		T entity = session.get(c, id);
+		session.close();
+		return entity;
+	}
+
+	public List<T> findAll()
+	{
+		Session session = sessionFactory.openSession();
+		List<T> entities = session.createQuery("from " + c.getName(), c).list();
+		session.close();
+		return entities;
+	}
+
+	public void delete(T entity)
+	{
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		try
+		{
+			session.delete(entity);
+			transaction.commit();
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			session.close();
+		}
+	}
+
+	public void deleteById(Long id)
+	{
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		delete(findById(id));
+		transaction.commit();
+		session.close();
+	}
+}
